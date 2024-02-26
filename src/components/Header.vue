0aa8f86969e48fab1dd67bb3e32afe170ec2d45d
David Adriazola <d.adriazolamuriel@gmail.com>
2024-02-26 17:25:08 -0400

    complete all sections

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,31 +4,31 @@
       class="mx-auto max-mx-auto max-w-container px-4 sm:px-6 lg:px-8-container flex justify-between items-center"
     >
       <div class="flex gap-2 items-center">
-        <img class="w-8" src="/src/assets/beerjs.svg" alt="beerjs" />
-        <h1 class="text-4xl font-bold text-white">
+        <img class="w-6 md:w-8" src="/src/assets/beerjs.svg" alt="beerjs" />
+        <h1 class="text-3xl md:text-4xl font-bold text-white">
           BEER <span class="text-yellow-500">JS</span>
         </h1>
       </div>
-      <div class="flex gap-5">
+      <div class="flex gap-3 md:gap-6">
         <a target="blank" href="https://www.facebook.com/beerjscbba"
           ><font-awesome-icon
             icon="fa-brands fa-facebook"
-            class="text-white text-3xl"
+            class="text-white text-2xl md:text-3xl"
         /></a>
         <a target="blank" href="https://twitter.com/beerjscbba"
           ><font-awesome-icon
             icon="fa-brands fa-x-twitter"
-            class="text-white text-3xl"
+            class="text-white text-2xl md:text-3xl"
         /></a>
         <a target="blank" href="https://www.instagram.com/beerjscbba/"
           ><font-awesome-icon
             icon="fa-brands fa-instagram"
-            class="text-white text-3xl"
+            class="text-white text-2xl md:text-3xl"
         /></a>
         <a target="blank" href="https://www.youtube.com/@beerjscocha7865">
           <font-awesome-icon
             icon="fa-brands fa-youtube"
-            class="text-white text-3xl"
+            class="text-white text-2xl md:text-3xl"
           />
         </a>
       </div>
